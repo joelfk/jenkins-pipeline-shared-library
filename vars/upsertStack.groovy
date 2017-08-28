585e9647ad32300a89f6fddb80bbecff8658ddea
Joel Kane <joel.kane@crownbet.com.au>
2017-08-28 17:15:44 +1000

    Fixing errors

--- vars/upsertStack.groovy
+++ vars/upsertStack.groovy
@@ -1,4 +1,5 @@
-def call(String name = 'human') {
-	def script = (libraryResource 'com/crownbet/upsertStack/upsertStack.sh').replace("{NAME}", "name")
-	sh(script)
+def call(String name = 'Human') {
+	def script = libraryResource 'com/crownbet/upsertStack/upsertStack.sh'
+	def script1 = script.replace('{NAME}', name)
+	sh script1
 }
