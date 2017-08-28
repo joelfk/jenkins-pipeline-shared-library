bcaa61e0e5e0722069e771d06c07f2491380f05e
Joel Kane <joel.kane@crownbet.com.au>
2017-08-28 17:07:44 +1000

    Removing redundant parts

--- vars/upsertStack.groovy
+++ vars/upsertStack.groovy
@@ -1,7 +1,4 @@
 def call(String name = 'human') {
 	def script = libraryResource 'com/crownbet/upsertStack/upsertStack.sh'
-	echo script
-	writeFile file: 'upsertStack.sh', text: script
-	sh 'ls -l'
 	sh script
 }
