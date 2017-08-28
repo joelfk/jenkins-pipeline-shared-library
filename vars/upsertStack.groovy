f5bedcb267f31ffb1a6e91e8303306cf646f9690
Joel Kane <joel.kane@crownbet.com.au>
2017-08-28 16:33:03 +1000

    Adding resource to shared library

--- vars/upsertStack.groovy
+++ vars/upsertStack.groovy
@@ -1,5 +1,5 @@
 def call(String name = 'human') {
-    // Any valid steps can be called from this code, just like in other
-    // Scripted Pipeline
-    echo "Hello, ${name}."
+	def script = libraryResource 'com/crownbet/upsertStack/upsertStack.sh'
+	writeFile file: 'upsertStack.sh', text: script
+	sh 'upsertStack.sh'
 }
