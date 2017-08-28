5ba0ea462fe763024aa7afaf0ec882814ea0b0d8
Joel Kane <joel.kane@crownbet.com.au>
2017-08-28 16:35:03 +1000

    Adding debugging

--- vars/upsertStack.groovy
+++ vars/upsertStack.groovy
@@ -1,5 +1,6 @@
 def call(String name = 'human') {
 	def script = libraryResource 'com/crownbet/upsertStack/upsertStack.sh'
+	echo script
 	writeFile file: 'upsertStack.sh', text: script
 	sh 'upsertStack.sh'
 }
